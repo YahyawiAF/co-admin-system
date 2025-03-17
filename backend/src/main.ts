3ee6fe04e75ccc6825254129a872dc2caf52a37a
yahyawiaf <abdelftt@gmail.com>
2025-03-17 10:10:53 +0100

    update

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -21,7 +21,11 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(4000);
 }
 bootstrap();
