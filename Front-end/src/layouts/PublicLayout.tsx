9484ab8d9bdc1a2362163ac0b4659ad32f809982
yahyawiaf <abdelftt@gmail.com>
2025-05-15 20:36:39 +0200

    update dev

--- Front-end/src/layouts/PublicLayout.tsx
+++ Front-end/src/layouts/PublicLayout.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import styled from "@emotion/styled";
-import "../global.css";
 
 import { CssBaseline, Paper as MuiPaper } from "@mui/material";
 import { ThemeProvider as MuiThemeProvider } from "@mui/material/styles";
